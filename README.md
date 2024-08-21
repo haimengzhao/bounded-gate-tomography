770933a3240388cbc450cd614419a06570f053c2
hmzhao <haimengzhao@icloud.com>
2024-08-21 22:14:47 +0800

    Update plots and README

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# bounded-gate-tomography
-Code for the paper "learning quantum states and unitaries of bounded gate complexity"
+# Learning quantum states and unitaries of bounded gate complexity
+This repo contains the code for the paper [Learning quantum states and unitaries of bounded gate complexity](https://arxiv.org/abs/2310.19882).
+
+The code implements the state learning algorithm detailed in the paper and study how its sample complexity $N$ scales with the gate size $G$.
+
+Thanks to the features of [TensorCircuit](https://github.com/tencent-quantum-lab/tensorcircuit) and [JAX](https://github.com/google/jax), our implementation support automatic vectorization and just-in-time (JIT) compilation.
+It can be executed on CPU, GPU, or TPU.
+
+## Requirements
+The code is written in Python3 and built upon [TensorCircuit](https://github.com/tencent-quantum-lab/tensorcircuit) and [JAX](https://github.com/google/jax).
+To execute it, we need to first install `numpy`, `jax`, `tensorcircuit`, `qiskit`, `tqdm`. To plot figures, we also need `matplotlib` and `SciencePlots`.
+They can all be installed using PyPI.
+
+## File Structure
+`circuit.py` contains elementary circuit functions including generating random circuit architectures and gate sequences, simulate circuits, computing fidelity, and junta learning.
+
+`shadow.py` implements Clifford classical shadow using JAX and TensorCircuit.
+One can choose to replace Clifford gates with Haar random unitaries to reduce statistical fluctuation.
+We also implement "shallow shadows" where global Clifford rotations are replaced by brickwork Clifford circuits with depth $\Theta(\log n)$.
+See [shallow shadows paper](https://arxiv.org/abs/2209.12924) and [shallow pseudorandom unitary paper](https://arxiv.org/abs/2407.07754) for details.
+
+`learn.py` implements the learning algorithm.
+
+`test.py` contains the pipeline for mass production that reproduces the data in the paper.
+
+`plot.py` plots the figures.
--- plot.py
+++ plot.py
@@ -1,59 +1,87 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import jax
-import jax.numpy as jnp
-from qiskit.quantum_info import random_clifford, random_unitary
 from tqdm import tqdm
-import tensorcircuit as tc
-K = tc.set_backend("jax")
 
 import scienceplots
 plt.style.use(['science','no-latex'])
 
-n = 10000
-focus_n = 4
-gate_set_size = 2
-sample_size_max = 70
-n_run = 20000
+# n = 10000
+# focus_n = 4
+# gate_set_size = 2
+# sample_size_max = 80
+# n_run = 100000
+# G_max = 10
+# G_list = range(1, G_max+1)
+# sample_size_list = np.arange(1, sample_size_max+1)
+
+# if focus_n is not None:
+#     fid_list = np.load(f"n={n}_G={G_max}_nrun={n_run}_focus.npy")
+# else:
+#     fid_list = np.load(f"n={n}_G={G_max}_nrun={n_run}.npy")
+
+levels = np.linspace(0, 1, 100)
+
 G_max = 10
+sample_size_max = 80
+fid_list = np.load(f"n=10000_G=10_nrun=100000_focus.npy")
 G_list = range(1, G_max+1)
 sample_size_list = np.arange(1, sample_size_max+1)
 
-
-
-if focus_n is not None:
-    fid_list = np.load(f"n={n}_G={G_max}_nrun={n_run}_focus.npy")
-else:
-    fid_list = np.load(f"n={n}_G={G_max}_nrun={n_run}.npy")
-
-fid_mean = jnp.mean(fid_list, axis=0)
-fid_med = jnp.quantile(fid_list, 0.5, axis=0)
+fid_mean = np.mean(fid_list, axis=0)
+fid_med = np.quantile(fid_list, 0.5, axis=0)
 fid_std = fid_list.std(axis=0)
 
 def median_of_mean(x, k):
     shape = x.shape
-    return jnp.median(x.reshape(k, -1, *shape[1:]).mean(axis=0), axis=0)
+    return np.median(x.reshape(k, -1, *shape[1:]).mean(axis=0), axis=0)
 
 fid_med_mean = median_of_mean(fid_list, 10)
 
-plt.figure(figsize=(8, 6))
-thres_list = [0.7, 0.8, 0.9, 0.95, 0.99]
+fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(7.5, 3))
+ax1, ax2 = axs
+
+thres_list = [0.95, 0.9, 0.8]
+
 for thres in thres_list:
     ind = np.array(np.argmax(fid_mean>thres, axis=-1))
-    plt.plot(G_list, sample_size_list[ind], linestyle='-', marker='o', label=f"$F={thres}$")
+    ax1.plot(G_list, sample_size_list[ind], linestyle='-', marker='o', label=f"$F={thres}$")
+
+cf1 = ax1.contourf(G_list, sample_size_list, fid_mean.T, levels=levels, vmin=0, vmax=1)
+
 ind = np.array(np.argmax(fid_med>0.999, axis=-1))
-plt.plot(G_list, sample_size_list[ind], linestyle='--', marker='o', label="$F_{med}=0.999$")
-plt.contourf(G_list, sample_size_list, fid_mean.T, levels=100)
-plt.colorbar()
-plt.xlabel("Gate Size $G$")
-# plt.xscale("log")
-plt.ylabel("Sample Size $N$")
-plt.title(f"$n={n}$, {n_run} repetitions")
-plt.legend()
+ax1.plot(G_list, sample_size_list[ind], linestyle='--', marker='o', label="$F_{med}=0.999$")
 
-if focus_n is not None:
-    plt.savefig(f"n={n}_G={G_max}_nrun={n_run}_focus.pdf")
-else:   
-    plt.savefig(f"n={n}_G={G_max}_nrun={n_run}.pdf")
+ax1.set_ylim(None, 50)
+ax1.set_xlabel("Gate Size $G$")
+ax1.set_ylabel("Sample Size $N$")
+ax1.legend()
 
-plt.show()
+G_max = 6
+sample_size_max = 60
+fid_list = np.load(f"n=10000_G=6_nrun=25000_shallow.npy")
+G_list = range(1, G_max+1)
+sample_size_list = np.arange(1, sample_size_max+1)
+
+fid_mean = np.mean(fid_list, axis=0)
+fid_med = np.quantile(fid_list, 0.5, axis=0)
+fid_std = fid_list.std(axis=0)
+
+for thres in thres_list:
+    ind = np.array(np.argmax(fid_mean>thres, axis=-1))
+    ax2.plot(G_list, sample_size_list[ind], linestyle='-', marker='o', label=f"$F={thres}$")
+
+ind = np.array(np.argmax(fid_med>0.999, axis=-1))
+ax2.plot(G_list, sample_size_list[ind], linestyle='--', marker='o', label="$F_{med}=0.999$")
+
+cf2 = ax2.contourf(G_list, sample_size_list, fid_mean.T, levels=levels, vmin=0, vmax=1)
+
+ax2.set_ylim(None, 50)
+ax2.set_xlabel("Gate Size $G$")
+ax2.set_ylabel("Sample Size $N$")
+ax2.legend()
+
+plt.tight_layout()
+plt.colorbar(cf1, ax=axs, ticks=np.arange(0, 1.1, 0.2))
+
+plt.savefig(f"numerics.pdf")
+
